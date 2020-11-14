0a3051b22ae867fe77d39ab90618eb222c7b2db3
pilotpirxie <10637666+pilotpirxie@users.noreply.github.com>
2020-11-14 02:55:45 +0100

    Prepare dark theme

--- src/components/WideSidebar.js
+++ src/components/WideSidebar.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 export default function WideSidebar({ children }) {
-  return <div className="mh-100 d-flex flex-column bg-light inspector-wrapper pt-4 p-1 shadow">
-    <div className="sidebar-wrapper container-fluid">
+  return <div className="mh-100 d-flex flex-column bg-light inspector-wrapper p-1">
+    <div className="sidebar-wrapper container-fluid p-1">
       {children}
     </div>
   </div>;
